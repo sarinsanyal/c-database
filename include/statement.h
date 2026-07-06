b9cea19874efee92579fa552da4084a9366be5aa
sarinsanyal <sarinsanyal2005@gmail.com>
2026-07-06 23:15:06 +0530

    added testing and made some major improvements

--- include/statement.h
+++ include/statement.h
@@ -12,6 +12,8 @@ typedef enum {
 typedef enum { 
     PREPARE_SUCCESS, 
     PREPARE_SYNTAX_ERROR,
+    PREPARE_NEGATIVE_ID,
+    PREPARE_STRING_TOO_LONG,
     PREPARE_UNRECOGNIZED_STATEMENT 
 } PrepareResult;
 
--- src/statement.c
+++ src/statement.c
@@ -1,26 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "statement.h"
+
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement){
+    (void) input_buffer;
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL)
+        return PREPARE_SYNTAX_ERROR;
+
+    int id = atoi(id_string);
+    if (id < 0){
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USERNAME_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement -> row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
 
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
     char* query = input_buffer->buffer;
 
     if (strncmp(query, "insert", 6) == 0 ){
-        statement->type = STATEMENT_INSERT;
-
-        int args_assigned = sscanf(
-            query, "insert %u %s %s", 
-            &(statement->row_to_insert.id),
-            statement->row_to_insert.username, 
-            statement->row_to_insert.email
-        );
-
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
+
     if (strcmp(query, "select") == 0) {
         statement->type = STATEMENT_SELECT;
         return PREPARE_SUCCESS;
